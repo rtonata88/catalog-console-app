dfb1614634d56f859d5714bb13b935fbc14f8ee9
rtonata88 <rtonata@gmail.com>
2022-03-22 20:01:45 +0200

    Add Genre class

--- Classes/item.rb
+++ Classes/item.rb
@@ -2,26 +2,18 @@ require 'date'
 
 class Item
   attr_accessor :archived
-
   # rubocop:enable Metrics/ParameterLists
-  #def initialize(id, genre, author, source, label, publish_date, archived: false)
-  def initialize(*args)
-    puts args[0]
-    args[0].each do |key,value|
-      singleton_class.class_eval { attr_accessor "#{key}" }
-      eval("@#{key} = #{value}")
-      end
-    # @id = id
-    # @genre = genre
-    # @author = author
-    # @source = source
-    # @label = label
-    # @publish_date = publish_date
-    # @archived = archived
+  def initialize(id, genre, author, source, label, publish_date, archived: false)
+    @id = id
+    @genre = genre
+    @author = author
+    @source = source
+    @label = label
+    @publish_date = publish_date
+    @archived = archived
     
   end
 
-  # rubocop:enable Metrics/ParameterLists
   def can_be_archived?
     publish_date = Date.parse(@publish_date)
     today = Date.today
@@ -32,3 +24,6 @@ class Item
     @archived = can_be_archived?
   end
 end
+
+item = Item.new(author: "Richard Chambula")
+puts item.author
