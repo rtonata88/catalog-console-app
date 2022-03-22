9fc27fd90fb12b4ff3e04173af7db804f2e3941f
Oliver Coimbra <olivercoimbra@gmail.com>
2022-03-22 11:07:22 -0400

    Fix linter errors

--- Classes/label.rb
+++ Classes/label.rb
@@ -1,18 +1,17 @@
 require_relative './item'
 
 class Label
-attr_reader :title, :color, :items, :id
+  attr_reader :title, :color, :items, :id
 
-def initialize(title, color, id = nil)
-@id = id || Random.rand(1..100)
-@title = title
-@color = color
-@items = []
-end
+  def initialize(title, color, id = nil)
+    @id = id || Random.rand(1..100)
+    @title = title
+    @color = color
+    @items = []
+  end
 
-def add_item(item)
-  @items << item
-  item.label = self
-end
-
+  def add_item(item)
+    @items << item
+    item.label = self
+  end
 end
